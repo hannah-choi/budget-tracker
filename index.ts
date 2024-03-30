4960131a2d6d525e819451d62a18a4354d0ac3e0
hannah-developer <hannahchoi.developer@gmail.com>
2024-03-30 17:29:13 +0000

    static file serving

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Response } from "express";
+import path from "path";
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hol0a");
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/", (_, res: Response) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+// app.get("/", (req: Request, res: Response) => {
+//     res.send("Hol0a");
+// });
 
 app.listen(port, () => {
-    console.log(`server  is listening on port ${port}`);
+    console.log(`server is listening on port ${port}`);
 });
